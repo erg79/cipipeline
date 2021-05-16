2cda7124e2a28a97ae2c4d8a1fa9794b89479ef5
Enrique Real <enrique.realginer@gmail.com>
2021-05-16 17:31:52 +0200

    app calculadora + tests

---
+++ com.calculadora.maven.eclipse/src/test/java/com/calculadora/com/calculadora/maven/eclipse/AppTest.java
@@ -0,0 +1,57 @@
+package com.calculadora.com.calculadora.maven.eclipse;
+
+import org.junit.*;
+import org.junit.rules.TestName;
+
+import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TestName;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+	/**
+	 * Regla de jUnit que guarda el nombre del test actual.
+	 */
+	public @Rule
+		TestName name = new TestName();
+
+	/**
+	 * Método que será invocado antes de ejecutar los test.
+	 */
+	@BeforeClass
+	public static void setUpClass(){
+		System.out.println("[ TESTS START ] - Comienzan a ejecutarse los Test Unitarios.");
+	}
+		
+	/**
+	 * Método que será invocados antes de cada test
+	 */
+	@Before
+	public void setUp(){
+		System.out.print("***********************************************************************************************************\n");
+		System.out.println("[ EJECUTANDO ] " + name.getMethodName());
+	}
+		
+	/**
+	 * Método que será invocados después de cada test
+	 */
+	@After
+	public void tearDown(){
+		System.out.println("[ TERMINADO ] " + name.getMethodName());
+		System.out.print("***********************************************************************************************************\n");
+	}
+
+	/**
+	 * Método que será invocado después de ejecutar los test.
+	 */
+	@AfterClass
+	public static void tearDownClass(){
+		System.out.println("[ TESTS FINISH ] -  Terminan de ejecutarse Test Unitarios.");
+	}
+	
+	
+}
